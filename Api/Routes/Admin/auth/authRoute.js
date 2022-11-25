a215ad6bd55ca94b590c7e0665fcec0ca8b4e38a
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-25 18:53:55 +0100

    add route for login admin

--- Api/Routes/Admin/auth/authRoute.js
+++ Api/Routes/Admin/auth/authRoute.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const router = express.Router();
-router.get('/login')
+const {login}=require('../../../Controllers/Admin/auth')
+router.get('/login',login)
 
 module.exports=router
