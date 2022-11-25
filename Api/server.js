908b4a714340d77ef403c98453864757d66cd0ff
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-25 16:27:22 +0100

    add body parser

--- Api/server.js
+++ Api/server.js
@@ -2,3 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
+
+//body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
