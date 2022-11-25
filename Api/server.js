025439830b91fce99e1273c240ff6230a9ecb6bd
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-25 16:28:11 +0100

    create PORT

--- Api/server.js
+++ Api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 //body parser
 app.use(express.json());
