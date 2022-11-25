b1ee5bb41b0e801eb5de25c5dd85ec6d74e436a9
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-25 19:31:03 +0100

    create clientSchema

---
+++ Api/Models/Client/Client.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const clientSchema = mongoose.Schema(
+  {
+    fname: {
+      type: String,
+      required: [true, "Please add a first name field"],
+    },
+    lname: {
+      type: String,
+      required: [true, "Please add a last name field"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add a email field"],
+      unique: true,
+    },
+    birthday: {
+      type: String,
+      required: [true, "Please add a birthday field"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Please add a phone field"],
+    },
+    adress: {
+      type: String,
+      required: [true, "Please add a adress field"],
+    },
+    city: {
+      type: String,
+      required: [true, "Please add a city field"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password field"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("Client", clientSchema);
