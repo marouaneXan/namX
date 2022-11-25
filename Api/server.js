e11dcaca3d7eeb2ede06188cf2bd4c0d8d82223b
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-25 16:34:20 +0100

    run server at the port 5000

--- Api/server.js
+++ Api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -10,3 +10,6 @@ app.use(express.urlencoded({ extended: false }));
 
 //cors 
 app.use(cors());
+
+//run server
+app.listen(PORT, () => console.log("Server running at the PORT " + PORT));
