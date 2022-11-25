4be36081b54690b51e175089b97d7e76050ff2df
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-25 19:16:17 +0100

    create generateToken for client

---
+++ Api/Utils/Client/generateToken.js
@@ -0,0 +1,7 @@
+//Generate JWT
+const generateToken = (client_id) => {
+    return jwt.sign({ client_id }, process.env.JWT_SECRET, {
+      expiresIn: "2h",
+    });
+  };
+  module.exports=generateToken
