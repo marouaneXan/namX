4a0599dd9995f6a86d731343a46a03f105c68e03
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-25 19:04:32 +0100

    create first version for login client

--- Api/Controllers/Client/auth.js
+++ Api/Controllers/Client/auth.js
@@ -3,12 +3,19 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 // @desc POST register
-// @route /api/v1/admin/auth/register
-// access private
+// @route /api/v1/client/auth/register
+// access public
 const register = asyncHandler(async (req, res) => {
     res.send('register')
+});
+// @desc POST register
+// @route /api/v1/client/auth/login
+// access public
+const login = asyncHandler(async (req, res) => {
+    res.send('login')
 });
 
 module.exports = {
     register,
+    login
   };
