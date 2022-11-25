66e4dcaab54492e1d195c980edf84f3d28b3f051
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-25 19:22:10 +0100

    rename auth file

--- Api/Routes/Admin/auth/authRoute.js
+++ Api/Routes/Admin/auth/authRoute.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const router = express.Router();
-const {login}=require('../../../Controllers/Admin/auth')
+const {login}=require('../../../Controllers/Admin/authController')
 router.get('/login',login)
 
 module.exports=router
