9b0bd4595801fa4ec67d0f4dba6c6e01853e777d
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-28 22:36:40 +0100

    make order

--- Api/Controllers/Order/orderController.js
+++ Api/Controllers/Order/orderController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler");
+const Car = require("../../Models/Car/Car");
+const Client = require("../../Models/Client/Client");
 const Order = require("../../Models/Order/Order");
 //@desc GET orders
 //@route /api/v1/orders
@@ -11,6 +13,32 @@ const getAllOrders = asyncHandler(async (req, res) => {
         message: "There is no orders yet",
       });
 });
+
+//@desc POST Order
+//@route /api/v1/orders
+//@access public
+const makeOrder = asyncHandler(async (req, res) => {
+  const time_now = new Date().toLocaleTimeString();
+  const date_now = new Date().toLocaleDateString("sv");
+  const car = await Car.findById(req.params.car_id);
+  const client = await Client.findById(req.params.client_id);
+  if (!car || !client) {
+    res.status(400);
+    throw new Error("Order failed");
+  }
+  const order = await Order.create({
+    order_date: date_now,
+    order_time: time_now,
+    car: car._id,
+    client: client._id,
+  });
+  if (order) {
+    res
+      .status(200)
+      .json({ message: "Your order has been confirmed successfully" });
+  } else res.status(400).json({ message: "Order failed" });
+});
 module.exports = {
   getAllOrders,
+  makeOrder
 };
