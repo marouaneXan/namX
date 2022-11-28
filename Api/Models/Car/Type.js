b350bbf2b426c106507d9ab0d60e3058ad6dda96
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-28 10:27:55 +0100

    create Type schema

---
+++ Api/Models/Car/Type.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const typeSchema = mongoose.Schema(
+  {
+    desc:{
+        type:String,
+        required:true,
+    },
+    details:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"Detail"
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("Type", typeSchema);
