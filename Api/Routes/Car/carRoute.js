a5707c21c5738ad677e281cbef3d99afc483c149
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-28 22:57:00 +0100

    update route for add car

--- Api/Routes/Car/carRoute.js
+++ Api/Routes/Car/carRoute.js
@@ -2,6 +2,6 @@ const express=require('express')
 const router = express.Router();
 const {addCar,getCars, updateCar, deleteCar}=require('../../Controllers/Car/carController')
 
-router.post('/',addCar).get('/',getCars).put('/:car_id',updateCar).delete('/:car_id',deleteCar)
+router.post('/:type_id',addCar).get('/',getCars).put('/:car_id',updateCar).delete('/:car_id',deleteCar)
 
 module.exports=router
