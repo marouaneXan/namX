fc8028708e7e08906325209f6603efb65afa5dd2
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-28 21:17:50 +0100

    add route for get all types

--- Api/Routes/Car/typeRoute.js
+++ Api/Routes/Car/typeRoute.js
@@ -1,7 +1,7 @@
-const express=require('express')
+const express=require('express');
 const router = express.Router();
-const {addType}=require('../../Controllers/Car/typeController')
+const {addType, getTypes}=require('../../Controllers/Car/typeController')
 
-router.post('/',addType)
+router.post('/',addType).get('/',getTypes)
 
 module.exports=router
