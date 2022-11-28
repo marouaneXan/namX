73b45283536ad2aff27b24aa8fc9ccdb58c4d7e4
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-28 21:29:06 +0100

    create orderSchema

---
+++ Api/Models/Order/Order.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose')
+const orderSchema=mongoose.Schema({
+    order_date:{
+        type:String,
+        required:true
+    },
+    order_time:{
+        type:String,
+        required:true
+    },
+    car:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"Car"
+    },
+    client:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"Client"
+    }
+})
+module.exports = mongoose.model("Order",orderSchema)
