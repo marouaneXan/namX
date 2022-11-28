fc526687a2bfa081450f9e49574b44f62b01c6f5
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-28 20:04:35 +0100

    addCar

---
+++ Api/Controllers/Car/carController.js
@@ -0,0 +1,26 @@
+const asyncHandler = require("express-async-handler");
+const Car = require("../../Models/Car/Car");
+
+// @desc POST addCar
+// @route /api/v1/cars
+// access private
+const addCar = asyncHandler(async (req, res) => {
+  const { car_name } = req.body;
+  if (!car_name) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+  const newCar = await Car.create(req.body);
+  if (newCar) {
+    res.status(200).json({
+      message: "car created successfully",
+    });
+  }else{
+    res.status(400);
+    throw new Error("Something wrong :(");
+  }
+});
+
+module.exports = {
+  addCar,
+};
