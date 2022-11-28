3b4a9d06354a32aee699e3e5cfc7a125897d013d
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-28 20:36:43 +0100

    update car

--- Api/Controllers/Car/carController.js
+++ Api/Controllers/Car/carController.js
@@ -29,7 +29,33 @@ const getCars = asyncHandler(async (req, res) => {
   res.status(200).json(cars);
 });
 
+//@desc PUT  Update car
+//@route /api/v1/cars/:car_id
+//@access private
+const updateCar = asyncHandler(async (req, res) => {
+  const car = await Car.findById(req.params.car_id);
+  if (!car) {
+    throw new Error("Car not found");
+  }
+  const updatedCar = await Car.findByIdAndUpdate(
+    req.params.car_id,
+    req.body,
+    {
+      new: true,
+    }
+  );
+  if (updatedCar) {
+    res.status(200).json({
+      message: "Car updated successfully",
+    });
+  }else{
+    res.status(400)
+    throw new Error('Something wrong :(')
+  }
+});
+
 module.exports = {
   addCar,
-  getCars
+  getCars,
+  updateCar
 };
