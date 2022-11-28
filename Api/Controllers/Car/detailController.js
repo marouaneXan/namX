4661a90db0926f0278c270baa3f4fe67a72f0345
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-28 11:29:05 +0100

    update details

--- Api/Controllers/Car/detailController.js
+++ Api/Controllers/Car/detailController.js
@@ -5,31 +5,56 @@ const Detail = require("../../Models/Car/Detail");
 // @route /api/v1/details
 // access private
 const addDetails = asyncHandler(async (req, res) => {
-    if(!req.body){
-        res.status(400)
-        throw new Error('Please add all fields')
+  if (!req.body) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+  const details = await Detail.create(req.body);
+  if (details) {
+    res.status(200).json({
+      message: "details created successfully",
+    });
+  } else {
+    res.status(400);
+    throw new Error("Something wrong :(");
+  }
+});
+
+//@desc PUT  Update detail
+//@route /api/v1/details/:detail_id
+//@access private
+const updateDetail = asyncHandler(async (req, res) => {
+  const detail = await Detail.findById(req.params.detail_id);
+  if (!detail) {
+    throw new Error("Detail not found");
+  }
+  const updatedDetail = await Detail.findByIdAndUpdate(
+    req.params.detail_id,
+    req.body,
+    {
+      new: true,
     }
-    const details=await Detail.create(req.body)
-    if(details){
-        res.status(200).json({
-            message:"details created successfully"
-        })
-    }else{
-        res.status(400)
-        throw new Error("Something wrong :(")
-    }
-})
+  );
+  if (updatedDetail) {
+    res.status(200).json({
+      message: "Detail updated successfully",
+    });
+  }else{
+    res.status(400)
+    throw new Error('Something wrong :(')
+  }
+});
 
 // @desc GET getDetails
 // @route /api/v1/details
 // access private
 const getDetails = asyncHandler(async (req, res) => {
-    const details=await Detail.find()
-    res.status(200).json(details)
-})
-
+  const details = await Detail.find();
+  res.status(200).json(details);
+});
 
 module.exports = {
-    addDetails,
-    getDetails
+  addDetails,
+  getDetails,
+  updateDetail,
 };
