3178015f77fca6632752b363f4c85a2625f52d8f
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-29 11:32:17 +0100

    protect car routes

--- Api/Routes/Car/carRoute.js
+++ Api/Routes/Car/carRoute.js
@@ -1,7 +1,8 @@
 const express=require('express')
 const router = express.Router();
 const {addCar,getCars, updateCar, deleteCar}=require('../../Controllers/Car/carController')
+const {protectAdmin}=require('../../Middleware/Admin/AdminMiddleware')
 
-router.post('/:type_id',addCar).get('/',getCars).put('/:car_id',updateCar).delete('/:car_id',deleteCar)
+router.post('/:type_id',protectAdmin,addCar).get('/',getCars).put('/:car_id',protectAdmin,updateCar).delete('/:car_id',protectAdmin,deleteCar)
 
 module.exports=router
