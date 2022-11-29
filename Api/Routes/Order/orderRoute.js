68615535738bd4e8cc383a0f7d4599d60044d958
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-29 09:39:03 +0100

    some changes

--- Api/Routes/Order/orderRoute.js
+++ Api/Routes/Order/orderRoute.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const router = express.Router();
-const {getAllOrders, makeOrder}=require('../../Controllers/Order/orderController')
+const {getAllOrders, makeOrder, cancelOrder}=require('../../Controllers/Order/orderController')
 
-router.get('/',getAllOrders).post('/:car_id/:client_id',makeOrder).delete('/:order_id/:car_id/:client_id')
+router.get('/',getAllOrders).post('/:car_id/:client_id',makeOrder).delete('/:order_id/:car_id/:client_id',cancelOrder)
 
 module.exports=router
