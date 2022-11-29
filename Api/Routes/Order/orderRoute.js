41c937e27e768fb4f00a12faa426af56f35f2d9f
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-29 13:54:06 +0100

    protect order routes

--- Api/Routes/Order/orderRoute.js
+++ Api/Routes/Order/orderRoute.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const router = express.Router();
 const {getAllOrders, makeOrder, cancelOrder}=require('../../Controllers/Order/orderController')
-
-router.get('/',getAllOrders).post('/:car_id/:client_id',makeOrder).delete('/:order_id/:car_id/:client_id',cancelOrder)
+const {protectAdmin}=require('../../Middleware/Admin/AdminMiddleware')
+router.get('/',protectAdmin,getAllOrders).post('/:car_id/:client_id',protectAdmin,makeOrder).delete('/:order_id/:car_id/:client_id',protectAdmin,cancelOrder)
 
 module.exports=router
