68f70175824bfe9372ca251c95e73ad49ecb18a1
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-29 09:08:17 +0100

    add function diff to calculate difference betwen two times

--- Api/Controllers/Order/orderController.js
+++ Api/Controllers/Order/orderController.js
@@ -38,6 +38,22 @@ const makeOrder = asyncHandler(async (req, res) => {
       .json({ message: "Your order has been confirmed successfully" });
   } else res.status(400).json({ message: "Order failed" });
 });
+
+// difference btween two times
+function diff(start, end) {
+  start = start.split(":");
+  end = end.split(":");
+  var startDate = new Date(0, 0, 0, start[0], start[1], 0);
+  var endDate = new Date(0, 0, 0, end[0], end[1], 0);
+  var diff = endDate.getTime() - startDate.getTime();
+  var hours = Math.floor(diff / 1000 / 60 / 60);
+  diff -= hours * 1000 * 60 * 60;
+  var minutes = Math.floor(diff / 1000 / 60);
+
+  return (
+    (hours <= 9 ? "0" : "") + hours + ":" + (minutes <= 9 ? "0" : "") + minutes
+  );
+}
 module.exports = {
   getAllOrders,
   makeOrder
