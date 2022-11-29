6d9bafa0dd847866859ece27dd10765c4fe9b78e
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-29 13:52:16 +0100

    protect type routes

--- Api/Routes/Car/typeRoute.js
+++ Api/Routes/Car/typeRoute.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router = express.Router();
 const {addType, getTypes, updateType, deleteType}=require('../../Controllers/Car/typeController')
-
-router.post('/',addType).get('/',getTypes).put('/:type_id',updateType).delete('/:type_id',deleteType)
+const {protectAdmin}=require('../../Middleware/Admin/AdminMiddleware')
+router.post('/',protectAdmin,addType).get('/',getTypes).put('/:type_id',protectAdmin,updateType).delete('/:type_id',protectAdmin,deleteType)
 
 module.exports=router
