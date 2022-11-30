487a2fcadf64ed71e14351e4a609d75d2da4f0c6
marouaneXan <marouanezahaoui835@gmail.com>
2022-11-30 14:09:22 +0100

    add route for get orders for one client

--- Api/Routes/Order/orderRoute.js
+++ Api/Routes/Order/orderRoute.js
@@ -1,8 +1,17 @@
-const express=require('express')
+const express = require("express");
 const router = express.Router();
-const {getAllOrders, makeOrder, cancelOrder}=require('../../Controllers/Order/orderController')
-const {protectAdmin}=require('../../Middleware/Admin/AdminMiddleware')
-const {protectClient}=require('../../Middleware/Client/ClientMiddleware')
-router.get('/',protectAdmin,getAllOrders).post('/:car_id/:client_id',protectClient,makeOrder).delete('/:order_id/:car_id/:client_id',protectClient,cancelOrder)
+const {
+  getAllOrders,
+  makeOrder,
+  cancelOrder,
+  getAllClientOrders,
+} = require("../../Controllers/Order/orderController");
+const { protectAdmin } = require("../../Middleware/Admin/AdminMiddleware");
+const { protectClient } = require("../../Middleware/Client/ClientMiddleware");
+router
+  .get("/", protectAdmin, getAllOrders)
+  .post("/:car_id/:client_id", protectClient, makeOrder)
+  .delete("/:order_id/:car_id/:client_id", protectClient, cancelOrder)
+  .get("/:client_id",getAllClientOrders)
 
-module.exports=router
+module.exports = router;
